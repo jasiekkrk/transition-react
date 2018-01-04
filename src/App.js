078ee99c61736e7348837146bfcb7eaa5a80e27e
Andrzej Jasinski <jasinski.andrzej@gmail.com>
2018-01-04 21:49:01 +0100

    zoomin

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import Hero from "./Hero";
 
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
+const transitionMapping = {
+  zoom: { classNames: 'zoomin', timeout: 500 },
+  hero: { classNames: 'zoomout', timeout: 500 },
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route render={({ location }) =>
-        <TransitionGroup>
-          <CSSTransition key={location.pathname.split('/')[1]} timeout={300} classNames="pageSlider" mountOnEnter={true} unmountOnExit={true}>
+         <Route render={({ location }) =>
+         {
+          const current = location.pathname.split('/')[1];
+           return <TransitionGroup>
+          <CSSTransition key={location.pathname.split('/')[1]} timeout={500}   {...transitionMapping[current]} mountOnEnter={true} unmountOnExit={true}>
             <Switch location={location}>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/zoom" component={Home} />
               <Route exact path="/hero" component={Hero} />
             </Switch>
           </CSSTransition>
         </TransitionGroup>
+         }
       } />
 
       </div>
--- src/Hero.js
+++ src/Hero.js
@@ -5,10 +5,12 @@ export default class Hero extends Component {
  render() {
   return (
    <div className="page hero">
-   <Link to="/" className='close'>X</Link>
+   <Link to="/zoom" className='close'>X</Link>
    <img className = "articleHero large" src="https://upload.wikimedia.org/wikipedia/commons/5/52/Krak%C3%B3w_239a.jpg"/>
-    <h1>Lost in krakow</h1>
-    <p>By Andrzej Jasiński</p>
+    <div className="hero-lablel">
+        <h1>Lost in krakow</h1>
+        <p>By Andrzej Jasiński</p>
+    </div>
    </div>
   )
  }
