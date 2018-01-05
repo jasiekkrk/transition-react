14936fa2c4ed68f640608f122d512a4d74831ad7
Andrzej Jasinski <andrzej.jasinski@fortedigital.no>
2018-01-05 09:40:43 +0100

    working zooming in animation

--- src/Hero.js
+++ src/Hero.js
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 export default class Hero extends Component {
  render() {
   return (
-   <div className="page hero">
-   <Link to="/zoom" className='close'>X</Link>
+    <div className="page hero">
+   Shared by you
    <img className = "articleHero large" src="https://upload.wikimedia.org/wikipedia/commons/5/52/Krak%C3%B3w_239a.jpg"/>
-    <div className="hero-lablel">
-        <h1>Lost in krakow</h1>
-        <p>By Andrzej Jasiński</p>
-    </div>
-   </div>
+   <h1>Lost in krakow</h1>
+   <Link to="/zoom" className='close'>X</Link>
+   <p>By Andrzej Jasiński</p>
+   </div>  
+
   )
  }
 }
