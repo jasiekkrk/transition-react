7baed71ff6ba70219803b9692ac9e895d3644c1d
Andrzej Jasinski <andrzej.jasinski@fortedigital.no>
2018-01-05 15:28:13 +0100

    no message

--- src/Hero.js
+++ src/Hero.js
@@ -5,13 +5,13 @@ export default class Hero extends Component {
  render() {
   return (
     <div className="page hero">
-   Shared by you
-   <img className = "articleHero large" src={require("./lksdv.jpg")}/>
-   <main>
-    <h1>Lost in krakow</h1>
-    <Link to="/" className='close'>X</Link>
-    <p>By John</p>
-    </main>
+        Shared by you
+        <img className = "articleHero large" src={require("./lksdv.jpg")}/>
+        <Link to="/" className='close'>X</Link>
+        <main>
+            <h1>Lost in krakow</h1>
+            <p>By John</p>
+        </main>
    </div>  
 
   )
