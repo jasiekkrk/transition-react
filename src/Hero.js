27a4aa2485e7cf245c3eeb745d0cf00284914c8b
Andrzej Jasinski <andrzej.jasinski@fortedigital.no>
2018-01-05 11:51:37 +0100

    no message

--- src/Hero.js
+++ src/Hero.js
@@ -9,9 +9,9 @@ export default class Hero extends Component {
    <div className = "articleHero large">
         <img  src="https://upload.wikimedia.org/wikipedia/commons/5/52/Krak%C3%B3w_239a.jpg"/>
    </div>
+   <Link to="/" className='close'>X</Link>
    <main>
     <h1>Lost in krakow</h1>
-    <Link to="/" className='close'>X</Link>
     <p>By John</p>
     </main>
    </div>  
